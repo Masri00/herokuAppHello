70e029260627f74f0c1e2508eed9ab277bb3f116
Hicham <hicham@bridgeathletic.com>
2021-07-30 12:55:26 +0300

    updated6

--- myFirstApp.py
+++ myFirstApp.py
@@ -5,12 +5,6 @@ st.write("""
 ## My first App
 """)
 
-number1=st.slider('Choose a number:', 1, 50,key='1')
-number2=st.slider('Choose a number:', 1, 50,key='2')
-number3=st.slider('Choose a number:', 1, 50,key='3')
-
-st.write(f"""# You chose {number1+number2+number3}""")
-st.write('hello1')
 with open('look_up.pkl', 'rb') as pkl_file:
     look_up_dictionary=pickle.load(pkl_file)
 oo='initial'
@@ -18,9 +12,12 @@ form = st.form(key='my_form')
 exercise=form.selectbox('select exrcise', ['Barbell bench press', 'Barbell back squat',
 'Barbell front squat','Barbell deadlift','Overhead press - standing barbell',
 'Barbell pendlay row'], key='exercise')
+dict=look_up_dictionary[exercise]
 gender=form.selectbox('select gender', ['male', 'female'], key='age')
-age=form.number_input('select age', min_value=1, max_value=10, value=5, step=1)
+dict=dict[gender]
+age=form.number_input('select age', min_value=1, max_value=50, value=20, step=1)
+dict=dict[age]
 submit_button = form.form_submit_button(label='Submit')
 if submit_button:
-    df=pd.DataFrame(look_up_dictionary[exercise][gender][age])
+    df=pd.DataFrame(dict)
     st.write(df.iloc[:10,:])
