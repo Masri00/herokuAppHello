4cbd567c4fedaf648845b130984c12498b0014c1
Hicham <hicham@bridgeathletic.com>
2021-07-30 13:18:04 +0300

    updated6

--- myFirstApp.py
+++ myFirstApp.py
@@ -4,9 +4,12 @@ import pandas as pd
 st.write("""
 ## My first App
 """)
-
-with open('look_up.pkl', 'rb') as pkl_file:
-    look_up_dictionary=pickle.load(pkl_file)
+@st.cache(allow_output_mutation=True)
+def get_look_up():
+    with open('look_up.pkl', 'rb') as pkl_file:
+        look_up_dictionary=pickle.load(pkl_file)
+    return look_up_dictionary
+look_up_dictionary=get_look_up()
 oo='initial'
 form = st.form(key='my_form')
 exercise=form.selectbox('select exrcise', ['Barbell bench press', 'Barbell back squat',
@@ -17,7 +20,8 @@ gender=form.selectbox('select gender', ['male', 'female'], key='age')
 dict=dict[gender]
 age=form.number_input('select age', min_value=1, max_value=50, value=20, step=1)
 dict=dict[age]
+df=pd.DataFrame(dict)
 submit_button = form.form_submit_button(label='Submit')
 if submit_button:
-    df=pd.DataFrame(dict)
+
     st.write(df.iloc[:10,:])
